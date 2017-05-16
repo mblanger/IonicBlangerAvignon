304406842ad890d996ec8323302e3b191a8f1a3d
Melissa Blanger <meblanger@dpt_info.iut.local>
2017-05-16 08:34:25 +0200

    Iniial commit

---
+++ src/pages/about/about.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+  providers: [Camera]
+})
+export class AboutPage {
+
+	public base64Image: string;
+  constructor(
+	public navCtrl: NavController,
+	public camera: Camera
+  
+  ) {
+
+  }
+  
+  takePicture(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
+}
